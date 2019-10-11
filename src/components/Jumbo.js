eab08d564c6ca8700b9fdc7d900575da0d69d5da
charlievillacr <charlievilla@gmail.com>
2019-10-10 20:32:36 -0600

    Jumbo.js component created

---
+++ src/components/Jumbo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyledJumbo } from '../styles/components'
+
+export default function Jumbo() {
+    return (
+        <StyledJumbo>
+           <div>
+            <h2>Find the best stuff here!</h2>  
+            <small>Designed & Styled by Daniela Gutierrez</small> 
+            </div> 
+        </StyledJumbo>
+    )
+}
